5e040187d7704c05534b509b18bb4e8d382d4cd4
Cedric Bonjour <Cedric Bonjour>
2017-01-31 13:05:41 -0500

    database initialized

--- Public/script.js
+++ Public/script.js
@@ -1,3 +1,59 @@
-function function_name(argument) {
-	// body...
+
+
+/*
+=========================
+handles different pages depending on the #
+=========================
+*/
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "Page/Home.html"
+    })
+    .when("/Advanced_Search", {
+        templateUrl : "Page/Advanced_Search.html"
+    })
+     .when("/About", {
+        templateUrl : "Page/About.html"
+    })
+    .when("/My_Sales", {
+        templateUrl : "Page/My_Sales.html"
+    })
+    .when("/Sign_In", {
+        templateUrl : "Page/Sign_In.html"
+    })
+    .when("/Sign_Up", {
+        templateUrl : "Page/Sign_Up.html"
+    })
+     .when("/Search_Results", {
+        templateUrl : "Page/Search_Results.html"
+    })
+ 
+   
+});
+
+function signUp(){
+    var email  = $("#signUpForm .email").val();
+    var password  = $("#signUpForm .password").val();
+    if(password!=$("#signUpForm .confirmPassword").val()){
+        console.log("password confirmation error not handled");
+    }else{
+      
+        $.ajax({url: "signup", type: 'POST', cache: false,  data: {email:email+".ca",password:password}, success: function(result){console.log(result)}});
+
+    }
+    
+
+}
+
+
+function getCookieValue(k){
+    var v=document.cookie.match('(^|;) ?'+k+'=([^;]*)(;|$)');
+    return v?v[2]:null;
+}
+
+function createCookie(){
+    document.cookie = "email="+email+";isLoggedIn=true";
 }
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser     =        require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('./Public', {}));
 
-// var path = require('path');
 
-// app.get("*", function (req, res) {
-//     res.statusCode = 404;
-//     res.setHeader("Content-Type", "application/json; charset=utf-8");
-//     res.end(JSON.stringify({error: "No such endpoint"}));
-// });
+
+var connection = mysql.createConnection({
+	host: 'sql9.freemysqlhosting.net',
+	user: 'sql9156464',
+	password: 'IU3pU1VTBH',
+	database : 'sql9156464'
+})
+
+
+
+
+app.post("/signup", function (req, res) {
+	var q1=req.body.email;
+	var q2=req.body.password;
+	
+	
+	
+	result  = connection.query('INSERT INTO `Member`(`email`, `password`) VALUES ("'+q1+'","'+q2+'")', function (error, results, fields) {
+		if(error){res.end(error.code)}else{
+			res.end("Successfully registered: "+q1);
+		}
+	});
+	
+	
+	
+});
+
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+	console.log('Example app listening on port 3000!');
+	connection.connect(function(err) {
+		if (err) {
+			console.error('error connecting: ' + err.stack);
+			return;
+		}
+
+		console.log('connected to database as id ' + connection.threadId);
+	});
 })
